36b93f15bceae9fde17922568ae72b885cb480c5
GabrielLourencoh <gl930551@gmail.com>
2025-06-05 18:18:52 -0300

    Criando novos Controllers, um de maneira manual e um de maneira automatica usando nest

--- src/conceitos-manual/conceitos-manual.module.ts
+++ src/conceitos-manual/conceitos-manual.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common"; // Import para usar o Module
+import { ConceitosManualController } from "./conceitos-manual.controller";
 
 // Criando o nosso modulo
 // Precisamos no app..module importar todas as modules criadas manualmente
-@Module({})
+@Module({
+    controllers: [ConceitosManualController],
+})
 export class ConceitosManualModule {}

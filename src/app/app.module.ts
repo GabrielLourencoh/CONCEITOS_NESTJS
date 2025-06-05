d76332575f2a2b1f567fb98de0aca71e4596af41
GabrielLourencoh <gl930551@gmail.com>
2025-06-05 16:35:26 -0300

    Criando um módulo de maneira automatica usando 'npm nest generate module conceitos-automatico'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceitosManualModule } from 'src/conceitos_manual/conceitos-manual.module';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 
 @Module({
-  imports: [ConceitosManualModule], // Importação de outros modulos
+  imports: [ConceitosManualModule, ConceitosAutomaticoModule], // Importação de outros modulos
   controllers: [AppController],
   providers: [AppService], // Injeção de Dependencias
   exports: [], // Do modulo raiz tambem podemos exportar 

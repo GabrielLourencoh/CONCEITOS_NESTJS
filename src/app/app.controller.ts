76d3b2fa2a6375e7afa236236d21619c21c01a12
GabrielLourencoh <gl930551@gmail.com>
2025-06-05 17:16:32 -0300

    Aprendendo um pouco sobre como funciona o controller e como modificar as rotas

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,13 +1,17 @@
 // Controla a request e response
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 
-@Controller()
+
+// Podemos informar um recurso tanto no controlador e tanto no recurso, entao, colocamos assim na url
+@Controller('home') // é o /home do site
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // Método da solicitação do http -> Ler (Read) 
+  // /home/hello
+  @Get('hello') // Isso é um recurso da solicitação
   getHello(): string {
-    return this.appService.getHello();
+    return 'Qualquer coisa';
   }
 }

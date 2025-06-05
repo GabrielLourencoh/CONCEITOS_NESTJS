de717e766a1968a1f08b712301eb03b6d6089758
GabrielLourencoh <gl930551@gmail.com>
2025-06-05 16:26:53 -0300

    Adicionando um modulo novo na mao e importando ele no nosso app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceitosManualModule } from 'src/conceitos_manual/conceitos-manual.module';
 
 @Module({
-  imports: [], // Importação de outros modulos
+  imports: [ConceitosManualModule], // Importação de outros modulos
   controllers: [AppController],
   providers: [AppService], // Injeção de Dependencias
   exports: [], // Do modulo raiz tambem podemos exportar 
---
+++ src/conceitos_manual/conceitos-manual.module.ts
@@ -0,0 +1,6 @@
+import { Module } from "@nestjs/common"; // Import para usar o Module
+
+// Criando o nosso modulo
+// Precisamos no app..module importar todas as modules criadas manualmente
+@Module({})
+export class ConceitosManualModule {}
